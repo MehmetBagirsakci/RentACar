44dc9bdd4b3c6e62c82ea6ce3cb82cc4f7576b58
MehmetBagirsakci <mehmetbagirsakci@gmail.com>
2023-11-09 17:34:09 +0300

    Add project files.

---
+++ Application/Features/Brands/Queries/GetList/GetListBrandListItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Queries.GetList;
+
+public class GetListBrandListItemDto
+{
+    //Veri kaynağındaki herşeyi kullanıcıya vermek istemiyoruz. Sadece kullanıcının görmesi gereken alanları ona gösteriyoruz.
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
+
---
+++ Persistence/EntityConfigurations/BrandConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.EntityConfigurations;
+
+public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+{
+    public void Configure(EntityTypeBuilder<Brand> builder)
+    {
+        builder.ToTable("Brands").HasKey(b=>b.Id);
+        builder.Property(b => b.Id).HasColumnName("Id").IsRequired(); //Id nin gelebilmesi için Core.Packages.Persistence'ın referansının buraya dahil edilmesi gerekir.
+        builder.Property(b => b.Name).HasColumnName("Name").IsRequired();
+        builder.Property(b => b.CreatedDate).HasColumnName("CreatedDate").IsRequired();
+        builder.Property(b => b.UpdatedDate).HasColumnName("UpdatedDate");
+        builder.Property(b => b.DeletedDate).HasColumnName("DeletedDate");
+
+        builder.HasQueryFilter(b => !b.DeletedDate.HasValue);//DeletedDate tarihinde veri yoksa (global filtre). Soft delete olanları getirme
+    }
+}
+//ToTable: nuget EntityFrameworkCore.SqlServer yada EntityFrameworkCore.InMemory den gelir
